18e487e22041faf825c442487fc4bceb93654fb1
Matt Johnson <matt@satchamo.com>
2019-07-28 12:17:37 -0700

    chmod .ssh/config

--- bin/initmyconf.sh
+++ bin/initmyconf.sh
@@ -8,3 +8,4 @@ GIT_SSH_COMMAND="ssh -i ~/.ssh/github.key" git clone --separate-git-dir=$HOME/.m
 rm -r ~/myconf-tmp/
 git --git-dir=$HOME/.myconf/ --work-tree=$HOME reset --hard HEAD
 git --git-dir=$HOME/.myconf/ --work-tree=$HOME config status.showUntrackedFiles no
+chmod 644 $HOME/.ssh/config
